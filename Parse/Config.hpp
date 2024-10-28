66d5bdab9dfec70b1c22bbcaa03ef5275f1b64f7
Carlos Eugenio Martin Gutierrez <carlos-m@c1r10s3.42madrid.com>
2024-10-28 13:41:24 +0100

    comienzo parseo

--- Parse/Config.hpp
+++ Parse/Config.hpp
@@ -12,4 +12,16 @@ class Config
 			Config(std::string const &defaultPath);
 			Config(Config const &copy);
 			~Config();
+
+		//exceptions
+			class NotExixtingPathException: public std::exception
+			{
+				public:
+					virtual const char *what() const throw()
+					{
+						return ("Configuration file error: Not existing file");
+					}
+			};
 };
+
+std::string	getConfFile(std::string const &defaultPath);
---
+++ Parse/configFunctions.cpp
@@ -0,0 +1,9 @@
+#include "Config.hpp"
+
+std::string	getConfFile(std::string const &defaultPath)
+{
+	std::string	content;
+
+	if (defaultPath.empty() || defaultPath.length() == 0)
+		throw ;
+}
